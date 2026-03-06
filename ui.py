acdf7cc6ad05c2f8262e4cef3fba549e52ece874
German Coronado <german_coronado@outlook.com>
2026-03-06 13:27:17 -0700

    Add edit position functionality

--- ui.py
+++ ui.py
@@ -45,6 +45,15 @@ class PlayerController:
             player.hits
         )
 
+    def edit_player_position(self, player: Player):
+        existing = db.get_player_by_id(player.playerID)
+        if existing is None:
+            return False
+
+        return db.db_edit_player_position(
+            player.playerID,
+            player.position
+        )
 
 
 # =====================================================
@@ -130,7 +139,7 @@ class PlayerApp(ttk.Frame):
         ttk.Button(right_frame, text="Remove Player", command=self.open_remove_player_window, width=button_width).grid(row=2, column=0, pady=5, sticky="ew")
         ttk.Button(right_frame, text="Move Player", width=button_width).grid(row=3, column=0, pady=5, sticky="ew")
         ttk.Button(right_frame, text="Edit Stats", command=self.open_edit_stats_window, width=button_width).grid(row=4, column=0, pady=5, sticky="ew")
-        ttk.Button(right_frame, text="Edit Position", width=button_width).grid(row=5, column=0, pady=5, sticky="ew")
+        ttk.Button(right_frame, text="Edit Position", command=self.open_edit_position_window, width=button_width).grid(row=5, column=0, pady=5, sticky="ew")
         ttk.Button(right_frame, text="Exit", command=self.parent.destroy,width=button_width).grid(row=6, column=0, pady=5, sticky="ew")
 
     # -----------------------------
@@ -247,6 +256,38 @@ class PlayerApp(ttk.Frame):
             command=popup.destroy   
         ).pack(side="left", padx=10)
 
+    def open_edit_position_window(self):
+        popup = tk.Toplevel(self)
+        popup.title("Edit Player Position")
+        popup.geometry("240x125")
+        popup.grid_columnconfigure(0, weight=0)
+        popup.grid_columnconfigure(1, weight=1)
+
+
+        ttk.Label(popup, text="Player ID").grid(row=0, column=0, padx=10, pady=(5, 5), sticky="w")
+        pid_var = tk.StringVar()
+        ttk.Entry(popup, textvariable=pid_var).grid(row=0, column=1, padx=10, pady=(5,5), sticky="ew")
+
+        ttk.Label(popup, text="Position").grid(row=1, column=0, padx=10, pady=(5, 5), sticky="w")
+        pos_var = tk.StringVar()
+        ttk.Entry(popup, textvariable=pos_var).grid(row=1, column=1, padx=10, pady=(5,5), sticky="ew")
+
+        button_frame = ttk.Frame(popup)
+        button_frame.grid(row=2, column=0, columnspan=2, pady=15)
+
+        ttk.Button(
+            button_frame,
+            text="Save",
+            command=lambda: self.save_player_position(
+                popup, pid_var.get(), pos_var.get()
+            )
+        ).pack(side="left", padx=10)
+
+        ttk.Button(
+            button_frame,
+            text="Cancel",
+            command=popup.destroy   
+        ).pack(side="left", padx=10)    
 
     # -----------------------------
     # Save and remove handlers
@@ -335,15 +376,42 @@ class PlayerApp(ttk.Frame):
         popup.destroy()
 
 
-    def save_player_position(self, popup, pid, first, last, pos):
-        # call your controller/db logic here
-        # db.db_add_player(...)
+    def save_player_position(self, popup, pid, pos):
+
+        try:
+            pid = int(pid)
+        except ValueError:
+            messagebox.showerror("Error", "Player ID, At Bats, and Hits must be numbers.")
+            return
+
+        if not pos.strip():
+            messagebox.showerror("Error", "Position cannot be empty.")
+            return
+        
+        else:
+            if pos not in db.VALID_POSITIONS:
+                messagebox.showerror("Error", f"Not a valid position. {db.VALID_POSITIONS}")
+                return
+            
+        player = Player(
+            playerID=pid,
+            position=pos
+        )
+
+        success = self.controller.edit_player_position(player)
+        if not success:
+            messagebox.showerror("Error", "Failed to edit player position.")
+            return
+        
+        self.load_players()
         messagebox.showinfo("Success", "Player Position modified.")
         popup.destroy()
     
     def save_moved_player(self, popup, pid, first, last, pos):
         # call your controller/db logic here
         # db.db_add_player(...)
+
+        self.load_players()
         messagebox.showinfo("Success", "Player moved.")
         popup.destroy()
 
